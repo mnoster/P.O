ffeee8235a487817770b868119fc286aec02d50d
Nicholas Porter <njporter10@gmail.com>
2016-10-04 10:27:52 -0700

    added in the new home page styling

--- form1.php
+++ form1.php
@@ -6,6 +6,12 @@ session_start();
         <h1 id="about_title">Client Form</h1>
         <form class="client_form">
             <h3 class='third_head'>Background Information</h3>
+            Age
+            <select name="Age" class="form-control" ng-model="selectedAge" ng-options="x for x in age">
+                <option disabled selected value> -- select an option --</option>
+
+            </select>
+            <br/>
             Ethnicity
             <select name="Ethnicity" class="form-control" ng-model="selectedEthnicity"
                     ng-options="x for x in ethnicity">
@@ -68,7 +74,66 @@ session_start();
                 <option> Yes</option>
                 <option> No</option>
             </select>
+            <br/>
+            Have you been arrested?
+            <select name="Veteran" class="form-control" ng-model="selectedArrested">
+                <option disabled selected value> -- select an option --</option>
+                <option> Yes</option>
+                <option> No</option>
+            </select>
+            <br/>
+            Have you been to prison?
+            <select name="Prison" class="form-control" ng-model="selectedPrison">
+                <option disabled selected value> -- select an option --</option>
+                <option> Yes</option>
+                <option> No</option>
+            </select>
+            <br/>
+            Do you have history of drug/alcohol abuse?
+            <select name="Veteran" class="form-control" ng-model="selectedDrugProblems">
+                <option disabled selected value> -- select an option --</option>
+                <option> Yes</option>
+                <option> No</option>
+            </select>
+            <br>
+            Have your mother or father suffered from mental health problems?
+            <select name="Parents" class="form-control" ng-model="selectedParents">
+                <option disabled selected value> -- select an option --</option>
+                <option> Yes</option>
+                <option> No</option>
+            </select>
+            <br>
+            Parental Situation
+            <select name="Veteran" class="form-control" ng-model="selectedVeteran">
+                <option disabled selected value> -- select an option --</option>
+                <option> Parents Married</option>
+                <option> Parents Divorced</option>
+                <option> Never Married </option>
+            </select>
+            <br>
+            Have you been without a place to live for more than 2 weeks?
+            <select name="Homeless" class="form-control" ng-model="selectedHomeless">
+                <option disabled selected value> -- select an option --</option>
+                <option> Yes</option>
+                <option> No</option>
+            </select>
+            <br>
+            How many days per week do you get more than 20 min of physical activity?
+            <select name="Activity" class="form-control" ng-model="selectedActivity">
+                <option disabled selected value> -- select an option --</option>
+                <option> 0 </option>
+                <option> 1 </option>
+                <option> 2 </option>
+                <option> 3 </option>
+                <option> 4 </option>
+                <option> 5 </option>
+                <option> 6 </option>
+                <option> 7 </option>
+            </select>
+            <br>
+            <br>
             <input type="submit">
+
         </form>
         <br>
         <form class="client_form second-form">
@@ -87,6 +152,7 @@ session_start();
             <br/>
 <!--            <diagnosis></diagnosis>-->
             <div class="container-fluid" ng-show="fc.oneField">
+                <hr>
                 <div class="row">
                     <div class="col-xs-3"><strong>Diagnosis 1 </strong></div>
                     <div class="col-xs-9">
@@ -97,11 +163,20 @@ session_start();
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-xs-3"><strong>Treatment 1</strong></div>
+                    <div class="col-xs-3"><strong>Therapy Treatment</strong></div>
                     <div class="col-xs-9">
-                        <select name="First Diagnosis" class="form-control" ng-model="firstTreatment"
-                                ng-options="x for x in past_diagnosis">
-                            <option disabled selected value> -- select a diagnosis --</option>
+                        <select name="First Treatment" class="form-control" ng-model="firstTreatment"
+                                ng-options="x for x in therapy_types">
+                            <option disabled selected value> -- select an option --</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-3"><strong>Type of Medication</strong></div>
+                    <div class="col-xs-9">
+                        <select name="First Medication" class="form-control" ng-model="firstMedication"
+                                ng-options="x for x in treatment_types">
+                            <option disabled selected value> -- select an option --</option>
                         </select>
                     </div>
                 </div>
@@ -118,7 +193,9 @@ session_start();
                 </div>
                 <br>
             </div>
+<!--            -----------------------two------------------------------------------>
             <div class="container-fluid" ng-show="fc.twoFields">
+                <hr>
                 <div class="row">
                     <div class="col-xs-3"><strong>Diagnosis 2 </strong></div>
                     <div class="col-xs-9">
@@ -129,11 +206,20 @@ session_start();
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-xs-3"><strong>Treatment 2</strong></div>
+                    <div class="col-xs-3"><strong>Therapy Treatment</strong></div>
                     <div class="col-xs-9">
-                        <select name="Second Diagnosis" class="form-control" ng-model="secondTreatment"
-                                ng-options="x for x in past_diagnosis">
-                            <option disabled selected value> -- select a diagnosis --</option>
+                        <select name="Second Treatment" class="form-control" ng-model="secondTreatment"
+                                ng-options="x for x in therapy_types">
+                            <option disabled selected value> -- select an option --</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-3"><strong>Type of Medication</strong></div>
+                    <div class="col-xs-9">
+                        <select name="Second Medication" class="form-control" ng-model="secondMedication"
+                                ng-options="x for x in treatment_types">
+                            <option disabled selected value> -- select an option --</option>
                         </select>
                     </div>
                 </div>
@@ -151,6 +237,7 @@ session_start();
                 <br>
             </div>
             <div class="container-fluid" ng-show="fc.threeFields">
+                <hr>
                 <div class="row">
                     <div class="col-xs-3"><strong>Diagnosis 3 </strong></div>
                     <div class="col-xs-9">
@@ -161,11 +248,20 @@ session_start();
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-xs-3"><strong>Treatment 3</strong></div>
+                    <div class="col-xs-3"><strong>Therapy Treatment</strong></div>
                     <div class="col-xs-9">
-                        <select name="Third Diagnosis" class="form-control" ng-model="thirdTreatment"
-                                ng-options="x for x in past_diagnosis">
-                            <option disabled selected value> -- select a diagnosis --</option>
+                        <select name="Third Treatment" class="form-control" ng-model="thirdTreatment"
+                                ng-options="x for x in therapy_types">
+                            <option disabled selected value> -- select an option --</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-3"><strong>Type of Medication</strong></div>
+                    <div class="col-xs-9">
+                        <select name="Third Medication" class="form-control" ng-model="thirdMedication"
+                                ng-options="x for x in treatment_types">
+                            <option disabled selected value> -- select an option --</option>
                         </select>
                     </div>
                 </div>
@@ -183,6 +279,7 @@ session_start();
                 <br>
             </div>
             <div class="container-fluid" ng-show="fc.fourFields">
+                <hr>
                 <div class="row">
                     <div class="col-xs-3"><strong>Diagnosis 4 </strong></div>
                     <div class="col-xs-9">
@@ -193,11 +290,20 @@ session_start();
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-xs-3"><strong>Treatment 4</strong></div>
+                    <div class="col-xs-3"><strong>Therapy Treatment</strong></div>
                     <div class="col-xs-9">
-                        <select name="Forth Diagnosis" class="form-control" ng-model="forthTreatment"
-                                ng-options="x for x in past_diagnosis">
-                            <option disabled selected value> -- select a diagnosis --</option>
+                        <select name="Forth Treatment" class="form-control" ng-model="forthTreatment"
+                                ng-options="x for x in therapy_types">
+                            <option disabled selected value> -- select an option --</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-3"><strong>Type of Medication</strong></div>
+                    <div class="col-xs-9">
+                        <select name="Forth Medication" class="form-control" ng-model="forthMedication"
+                                ng-options="x for x in treatment_types">
+                            <option disabled selected value> -- select an option --</option>
                         </select>
                     </div>
                 </div>
@@ -215,6 +321,7 @@ session_start();
                 <br>
             </div>
             <div class="container-fluid" ng-show="fc.fiveFields">
+                <hr>
                 <div class="row">
                     <div class="col-xs-3"><strong>Diagnosis 5 </strong></div>
                     <div class="col-xs-9">
@@ -225,11 +332,20 @@ session_start();
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-xs-3"><strong>Treatment 5</strong></div>
+                    <div class="col-xs-3"><strong>Therapy Treatment</strong></div>
                     <div class="col-xs-9">
-                        <select name="Fifth Diagnosis" class="form-control" ng-model="fifthTreatment"
-                                ng-options="x for x in past_diagnosis">
-                            <option disabled selected value> -- select a diagnosis --</option>
+                        <select name="Fifth Treatment" class="form-control" ng-model="fifthTreatment"
+                                ng-options="x for x in therapy_types">
+                            <option disabled selected value> -- select an option --</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-3"><strong>Type of Medication</strong></div>
+                    <div class="col-xs-9">
+                        <select name="Fifth Medication" class="form-control" ng-model="fifthMedication"
+                                ng-options="x for x in treatment_types">
+                            <option disabled selected value> -- select an option --</option>
                         </select>
                     </div>
                 </div>
@@ -247,6 +363,7 @@ session_start();
                 <br>
             </div>
             <div class="container-fluid" ng-show="fc.sixFields">
+                <hr>
                 <div class="row">
                     <div class="col-xs-3"><strong>Diagnosis 6 </strong></div>
                     <div class="col-xs-9">
@@ -257,11 +374,20 @@ session_start();
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-xs-3"><strong>Treatment 6</strong></div>
+                    <div class="col-xs-3"><strong>Therapy Treatment</strong></div>
                     <div class="col-xs-9">
-                        <select name="Sixth Diagnosis" class="form-control" ng-model="sixthTreatment"
-                                ng-options="x for x in past_diagnosis">
-                            <option disabled selected value> -- select a diagnosis --</option>
+                        <select name="Sixth Treatment" class="form-control" ng-model="sixthTreatment"
+                                ng-options="x for x in therapy_types">
+                            <option disabled selected value> -- select an option --</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-3"><strong>Type of Medication</strong></div>
+                    <div class="col-xs-9">
+                        <select name="Sixth Medication" class="form-control" ng-model="sixthMedication"
+                                ng-options="x for x in treatment_types">
+                            <option disabled selected value> -- select an option --</option>
                         </select>
                     </div>
                 </div>
@@ -279,6 +405,7 @@ session_start();
                 <br>
             </div>
             <div class="container-fluid" ng-show="fc.sevenFields">
+                <hr>
                 <div class="row">
                     <div class="col-xs-3"><strong>Diagnosis 7 </strong></div>
                     <div class="col-xs-9">
@@ -289,11 +416,20 @@ session_start();
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-xs-3"><strong>Treatment 7</strong></div>
+                    <div class="col-xs-3"><strong>Therapy Treatment</strong></div>
                     <div class="col-xs-9">
-                        <select name="Seventh Diagnosis" class="form-control" ng-model="seventhTreatment"
-                                ng-options="x for x in past_diagnosis">
-                            <option disabled selected value> -- select a diagnosis --</option>
+                        <select name="Seventh Treatment" class="form-control" ng-model="seventhTreatment"
+                                ng-options="x for x in therapy_types">
+                            <option disabled selected value> -- select an option --</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-3"><strong>Type of Medication</strong></div>
+                    <div class="col-xs-9">
+                        <select name="Seventh Medication" class="form-control" ng-model="seventhMedication"
+                                ng-options="x for x in treatment_types">
+                            <option disabled selected value> -- select an option --</option>
                         </select>
                     </div>
                 </div>
@@ -311,6 +447,7 @@ session_start();
                 <br>
             </div>
             <div class="container-fluid" ng-show="fc.eightFields">
+                <hr>
                 <div class="row">
                     <div class="col-xs-3"><strong>Diagnosis 8 </strong></div>
                     <div class="col-xs-9">
@@ -321,11 +458,20 @@ session_start();
                     </div>
                 </div>
                 <div class="row">
-                    <div class="col-xs-3"><strong>Treatment 8</strong></div>
+                    <div class="col-xs-3"><strong>Therapy Treatment</strong></div>
                     <div class="col-xs-9">
-                        <select name="Eighth Diagnosis" class="form-control" ng-model="eighthTreatment"
-                                ng-options="x for x in past_diagnosis">
-                            <option disabled selected value> -- select a diagnosis --</option>
+                        <select name="Eighth Treatment" class="form-control" ng-model="eighthTreatment"
+                                ng-options="x for x in therapy_types">
+                            <option disabled selected value> -- select an option --</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-xs-3"><strong>Type of Medication</strong></div>
+                    <div class="col-xs-9">
+                        <select name="Eighth Medication" class="form-control" ng-model="eighthMedication"
+                                ng-options="x for x in treatment_types">
+                            <option disabled selected value> -- select an option --</option>
                         </select>
                     </div>
                 </div>
