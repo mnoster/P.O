5ffa6351f1e02311d2c905c4594f6bb40d5cc0ed
Nicholas Porter <njporter10@gmail.com>
2016-10-04 14:09:33 -0700

    made gianormous query for the client form

--- script.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by njporter10 on 6/27/16.
- */
-// $(function(){
-//     var $select = $(".1-100");
-//     for (i=1;i<=100;i++){
-//         $select.append($('<option></option>').val(i).html(i))
-//     }
-// });​
