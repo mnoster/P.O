b709547b338d11f96a3e651a3de870ab8383d412
Nicholas Porter <njporter10@gmail.com>
2016-10-11 20:38:33 -0700

    added in the new styling for the second info section on the new home page

--- main-page.php
+++ main-page.php
@@ -9,14 +9,16 @@
                     <h1 data-aos="fade-up"  style="margin:0; padding:20px 0 0 45px"><img src="Images/Logomakr_0X15Cd.png"></h1>
                     <h1  data-aos="fade-down" class="title">Psych Origins</h1>
                     <hr>
-                    <div class="intro"  data-aos="flip-up" data-aos-easing="ease-in-sine">Open Source academic database for researchers and mental health clinicians</div>
-                    <h1  class="glyphicon glyphicon-chevron-down"></h1>
+                    <div class="intro"  data-aos="flip-up" data-aos-easing="ease-in-sine">Open-source academic database for researchers and mental health clinicians</div>
+                    <h1 class="glyphicon glyphicon-chevron-down"></h1>
                 </div>
         </div>
     </div>
-<h1 style="color:black">Welcome to PsychOrigins.com</h1>
+    <div class="info1" data-aos="fade-up">
+        Access hundreds of thousands of research articles through the<span> PsychOrigins Academic Search</span>&trade; engine. Powered by <img src="Images/micosoftlogo.png" height="32px">  Microsoft's Academic Knowledge database, all research papers are available for viewing without cost.
+    </div>
 </div>
-<div id="wrapper">
+<div class="wrapper">
     <div class="wrapper-inner">
         <div class="bg-middle">
         </div>
