3d16773953fac53bc372ee27b2d88c36d86e332c
Nicholas Porter <njporter10@gmail.com>
2016-12-05 20:27:18 -0800

    working on search params

--- index.php
+++ index.php
@@ -61,9 +61,9 @@ if (isset($_SESSION['active'])) {
                 </li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-                <li  ng-show="active"><a href="#logout"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
-                <li  ng-hide="active"><a href="http://psychorigins.com/login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
-                <li  ng-hide="active"><a href="#register">Register</a></li>
+                <li ng-show="active"><a href="#logout"><span class="glyphicon glyphicon-log-in"></span> Logout</a></li>
+                <li ng-hide="active"><a href="http://psychorigins.com/login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                <li ng-hide="active"><a href="#register">Register</a></li>
             </ul>
         </div>
     </div>
